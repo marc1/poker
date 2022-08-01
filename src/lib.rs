267fcf3b2f061d8f9db46d3f1293a0593edee781
marc1 <m@marc.rs>
2022-07-31 18:29:00 -0700

    implement comparison and ordering for Rank, Suit, Card

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![allow(warnings)]
 
-use std::fmt::Display;
+use std::{
+    cmp::Ordering,
+    fmt::Display
+};
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Rank {
     Two,
     Three,
@@ -18,6 +22,7 @@ enum Rank {
     Ace,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Suit {
     Club,
     Diamond,
@@ -25,6 +30,7 @@ enum Suit {
     Spade,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 struct Card {
     rank: Rank,
     suit: Suit,
@@ -40,12 +46,34 @@ impl Card {
     }
 }
 
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.rank() == other.rank() {
+            self.suit().cmp(&other.suit())
+        } else {
+            self.rank().cmp(&other.rank())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{Rank, Suit, Card};
 
     #[test]
-    fn foo() {
+    fn card_cmp() {
+        let c1 = Card { rank: Rank::Ace, suit: Suit::Spade };
+        let c2 = Card { rank: Rank::Ace, suit: Suit::Club };
 
+        let c3 = Card { rank: Rank::Ten, suit: Suit::Spade };
+
+        assert!(c1 > c2);
+        assert!(c2 > c3);
     }
 }
